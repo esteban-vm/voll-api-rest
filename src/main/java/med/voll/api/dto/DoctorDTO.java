6ae9990f822eaddb5f176cb73ccca65799a8fb37
Esteban V.M. <estebanvm90@outlook.com>
2025-06-28 16:42:41 -0300

    update DoctorDTO

--- src/main/java/med/voll/api/dto/DoctorDTO.java
+++ src/main/java/med/voll/api/dto/DoctorDTO.java
@@ -1,12 +1,34 @@
 package med.voll.api.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.enums.MedicalSpecialty;
 
 public record DoctorDTO(
+
+        @NotBlank
         String name,
+
+        @NotBlank
+        @Email
         String email,
+
+        @NotBlank
+        String phone,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{7,9}")
         String document,
+
+        @NotNull
         MedicalSpecialty specialty,
+
+        @NotNull
+        @Valid
         AddressDTO address
+        
 ) {
 }
