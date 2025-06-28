2cba3cf993cd3ace57e7604d11954bee96423b09
Esteban V.M. <estebanvm90@outlook.com>
2025-06-28 16:42:53 -0300

    update DoctorController

--- src/main/java/med/voll/api/controllers/DoctorController.java
+++ src/main/java/med/voll/api/controllers/DoctorController.java
@@ -1,6 +1,11 @@
 package med.voll.api.controllers;
 
+import jakarta.validation.Valid;
 import med.voll.api.dto.DoctorDTO;
+import med.voll.api.models.Doctor;
+import med.voll.api.repositories.DoctorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/doctors")
 public class DoctorController {
 
+    @Autowired
+    private DoctorRepository repository;
+
+    @Transactional
     @PostMapping
-    public void register(@RequestBody DoctorDTO doctor) {
-        System.out.println(doctor);
+    public void register(@RequestBody @Valid DoctorDTO doctor) {
+        repository.save(new Doctor(doctor));
     }
 
 }
