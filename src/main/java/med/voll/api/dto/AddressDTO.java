b6d7f6195bbd9f2dbe8a1335b9d582f97ed33c60
Esteban V.M. <estebanvm90@outlook.com>
2025-06-28 16:41:47 -0300

    update AddressDTO

--- src/main/java/med/voll/api/dto/AddressDTO.java
+++ src/main/java/med/voll/api/dto/AddressDTO.java
@@ -1,12 +1,28 @@
 package med.voll.api.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public record AddressDTO(
+
+        @NotBlank
         String street,
+
         String number,
         String complement,
+
+        @NotBlank
         String neighbor,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{4}")
         String postal_code,
+
+        @NotBlank
         String city,
+
+        @NotBlank
         String state
+
 ) {
 }
