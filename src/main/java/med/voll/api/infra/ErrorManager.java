010fa3ff818cfb0c841a7deb2e67affb8e921f9f
Esteban V.M. <estebanvm90@outlook.com>
2025-07-03 14:05:02 -0300

    create ErrorManager

---
+++ src/main/java/med/voll/api/infra/ErrorManager.java
@@ -0,0 +1,31 @@
+package med.voll.api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorManager {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity handleNotFound() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handleBadRequest(MethodArgumentNotValidException exception) {
+        var errors = exception.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(ValidationErrorData::new).toList());
+    }
+
+    private record ValidationErrorData(String field, String message) {
+
+        public ValidationErrorData(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+
+    }
+}
