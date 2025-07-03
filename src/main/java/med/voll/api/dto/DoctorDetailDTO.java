0bf850ec2f318bf9fb1936b5afa0003e0211a290
Esteban V.M. <estebanvm90@outlook.com>
2025-07-03 12:57:50 -0300

    create DoctorDetailDTO

---
+++ src/main/java/med/voll/api/dto/DoctorDetailDTO.java
@@ -0,0 +1,30 @@
+package med.voll.api.dto;
+
+import med.voll.api.enums.MedicalSpecialty;
+import med.voll.api.models.Address;
+import med.voll.api.models.Doctor;
+
+public record DoctorDetailDTO(
+
+        Long id,
+        String name,
+        String email,
+        String phone,
+        String document,
+        MedicalSpecialty specialty,
+        Address address
+
+) {
+
+    public DoctorDetailDTO(Doctor doctor) {
+        this(
+                doctor.getId(),
+                doctor.getName(),
+                doctor.getEmail(),
+                doctor.getPhone(),
+                doctor.getDocument(),
+                doctor.getSpecialty(),
+                doctor.getAddress()
+        );
+    }
+}
