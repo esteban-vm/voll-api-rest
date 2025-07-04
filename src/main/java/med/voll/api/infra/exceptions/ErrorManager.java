ddabcd3a8c14dab29fa6a46200134776457501cf
Esteban V.M. <estebanvm90@outlook.com>
2025-07-04 13:18:33 -0300

    update ErrorManager

--- src/main/java/med/voll/api/infra/exceptions/ErrorManager.java
+++ src/main/java/med/voll/api/infra/exceptions/ErrorManager.java
@@ -1,11 +1,17 @@
 package med.voll.api.infra.exceptions;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.naming.AuthenticationException;
+import java.nio.file.AccessDeniedException;
 
 @RestControllerAdvice
 public class ErrorManager {
@@ -21,6 +27,31 @@ public class ErrorManager {
         return ResponseEntity.badRequest().body(errors.stream().map(ValidationErrorData::new).toList());
     }
 
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity handleBadRequest(HttpMessageNotReadableException exception) {
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity handleBadCredentials() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciales inválidas");
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity handleUnauthorized() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Falla en la autenticación");
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity handleAccessDenied() {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Acceso denegado");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity handleServerError(Exception exception) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error: " + exception.getLocalizedMessage());
+    }
+
     private record ValidationErrorData(String field, String message) {
 
         public ValidationErrorData(FieldError error) {
@@ -28,4 +59,5 @@ public class ErrorManager {
         }
 
     }
+
 }
