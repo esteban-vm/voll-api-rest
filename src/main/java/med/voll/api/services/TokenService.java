1b9f6a520ae60cfacc729c54633a1a9f97d5fcd4
Esteban V.M. <estebanvm90@outlook.com>
2025-07-04 13:17:55 -0300

    update TokenService

--- src/main/java/med/voll/api/services/TokenService.java
+++ src/main/java/med/voll/api/services/TokenService.java
@@ -3,6 +3,7 @@ package med.voll.api.services;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.voll.api.models.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -14,20 +15,23 @@ import java.time.ZoneOffset;
 @Service
 public class TokenService {
 
+    private static final String ISSUER = "API Voll.med";
+
     @Value("${api.security.token.secret}")
     private String secret;
 
     public String generateToken(User user) {
         try {
             var algorithm = Algorithm.HMAC256(secret);
+
             return JWT.create()
-                    .withIssuer("API Voll.med")
+                    .withIssuer(ISSUER)
                     .withSubject(user.getUsername())
                     .withExpiresAt(expirationDate())
                     .withClaim("id", user.getId())
                     .sign(algorithm);
+
         } catch (JWTCreationException exception) {
-            // Invalid Signing configuration / Couldn't convert Claims.
             throw new RuntimeException("Error al generar el token JWT", exception);
         }
     }
@@ -36,6 +40,21 @@ public class TokenService {
         return LocalDateTime.now()
                 .plusHours(2)
                 .toInstant(ZoneOffset.of("-03:00"));
+    }
+
+    public String getSubject(String jwtToken) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.require(algorithm)
+                    .withIssuer(ISSUER)
+                    .build()
+                    .verify(jwtToken)
+                    .getSubject();
+
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido o expirado", exception);
+        }
     }
 
 }
