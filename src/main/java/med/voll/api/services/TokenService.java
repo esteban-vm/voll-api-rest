fc6789776c8336c8e96926f2c2048d2728251576
Esteban V.M. <estebanvm90@outlook.com>
2025-07-04 10:50:05 -0300

    create TokenService

---
+++ src/main/java/med/voll/api/services/TokenService.java
@@ -0,0 +1,41 @@
+package med.voll.api.services;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import med.voll.api.models.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(User user) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("API Voll.med")
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(expirationDate())
+                    .withClaim("id", user.getId())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception) {
+            // Invalid Signing configuration / Couldn't convert Claims.
+            throw new RuntimeException("Error al generar el token JWT", exception);
+        }
+    }
+
+    private Instant expirationDate() {
+        return LocalDateTime.now()
+                .plusHours(2)
+                .toInstant(ZoneOffset.of("-03:00"));
+    }
+
+}
