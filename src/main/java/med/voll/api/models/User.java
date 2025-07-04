aebdf0777a756154fc598b7209cb49157206022a
Esteban V.M. <estebanvm90@outlook.com>
2025-07-03 22:39:34 -0300

    create User model

---
+++ src/main/java/med/voll/api/models/User.java
@@ -0,0 +1,24 @@
+package med.voll.api.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "users")
+@Entity(name = "User")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String username;
+    private String password;
+
+}
