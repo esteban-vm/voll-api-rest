8ca0fef83081ecd70bd4438aa1dc652d131dcf96
Esteban V.M. <estebanvm90@outlook.com>
2025-07-08 12:54:29 -0300

    update AppointmentCreateDTO

--- src/main/java/med/voll/api/dto/AppointmentCreateDTO.java
+++ src/main/java/med/voll/api/dto/AppointmentCreateDTO.java
@@ -2,17 +2,22 @@ package med.voll.api.dto;
 
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
+import med.voll.api.enums.MedicalSpecialty;
 
 import java.time.LocalDateTime;
 
 public record AppointmentCreateDTO(
 
         Long idDoctor,
+
         @NotNull
         Long idPatient,
+
         @NotNull
         @Future
-        LocalDateTime dateTime
-        
+        LocalDateTime dateTime,
+
+        MedicalSpecialty specialty
+
 ) {
 }
