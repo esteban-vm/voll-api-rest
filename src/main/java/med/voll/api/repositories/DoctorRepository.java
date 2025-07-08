b45a58fc4a2b1c446d55771e5d831efa6d5a2f8a
Esteban V.M. <estebanvm90@outlook.com>
2025-07-08 12:54:48 -0300

    update DoctorRepository

--- src/main/java/med/voll/api/repositories/DoctorRepository.java
+++ src/main/java/med/voll/api/repositories/DoctorRepository.java
@@ -1,12 +1,34 @@
 package med.voll.api.repositories;
 
+import med.voll.api.enums.MedicalSpecialty;
 import med.voll.api.models.Doctor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
 
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
 
     Page<Doctor> findAllByActiveTrue(Pageable pageable);
+
+    @Query(value = """
+            select d
+            from Doctor d
+            where
+            d.active = true
+            and
+            d.specialty = :specialty
+            and
+            d.id not in(
+                select a.doctor.id from Appointment a
+                where
+                a.dateTime = :dateTime
+            )
+            order by rand()
+            limit 1
+            """)
+    Doctor chooseRandomDoctorAvailableOnDate(MedicalSpecialty specialty, LocalDateTime dateTime);
 
 }
