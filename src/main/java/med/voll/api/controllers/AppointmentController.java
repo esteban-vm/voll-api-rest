9df04d52fd3a00273adf536798f27d51ee7d9c09
Esteban V.M. <estebanvm90@outlook.com>
2025-07-08 12:54:17 -0300

    update AppointmentController

--- src/main/java/med/voll/api/controllers/AppointmentController.java
+++ src/main/java/med/voll/api/controllers/AppointmentController.java
@@ -2,23 +2,33 @@ package med.voll.api.controllers;
 
 import jakarta.validation.Valid;
 import med.voll.api.dto.AppointmentCreateDTO;
+import med.voll.api.dto.AppointmentDeleteDTO;
 import med.voll.api.dto.AppointmentDetailDTO;
+import med.voll.api.services.AppointmentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/appointments")
 public class AppointmentController {
 
+    @Autowired
+    private AppointmentService service;
+
     @Transactional
     @PostMapping
     public ResponseEntity<AppointmentDetailDTO> create(@RequestBody @Valid AppointmentCreateDTO dto) {
-        System.out.println(dto);
+        service.book(dto);
         return ResponseEntity.ok(new AppointmentDetailDTO(null, null, null, null));
+    }
+
+    @Transactional
+    @DeleteMapping
+    public ResponseEntity cancel(@RequestBody @Valid AppointmentDeleteDTO dto) {
+        service.cancel(dto);
+        return ResponseEntity.noContent().build();
     }
 
 }
