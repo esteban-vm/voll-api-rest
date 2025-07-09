d23d22dafeb544ae4ee9432958f432faef60c5bc
Esteban V.M. <estebanvm90@outlook.com>
2025-07-09 09:26:17 -0300

    update PatientRepository

--- src/main/java/med/voll/api/repositories/PatientRepository.java
+++ src/main/java/med/voll/api/repositories/PatientRepository.java
@@ -4,11 +4,18 @@ import med.voll.api.models.Patient;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
     Page<Patient> findAllByActiveTrue(Pageable pageable);
 
+    @Query(value = """
+            select p.active
+            from Patient p
+            where
+            p.id = :idPatient
+            """)
     Boolean findActiveById(Long idPatient);
 
 }
